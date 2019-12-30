2dbd39d8864ab6167bea8d58d2543050dc622768
Francois MUGOROZI <53614123+Francois-MUGOROZI@users.noreply.github.com>
2019-12-30 08:09:57 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,17 +5,6 @@ import  firebase from 'firebase';
 import App from './App';
 
 
-const config = {
-    apiKey: "AIzaSyCHTk8REcfmeK_k7cM5OO-yFcnY0V_Ytro",
-    authDomain: "shared-task-63bbc.firebaseapp.com",
-    databaseURL: "https://shared-task-63bbc.firebaseio.com",
-    projectId: "shared-task-63bbc",
-    storageBucket: "shared-task-63bbc.appspot.com",
-    messagingSenderId: "450230086080",
-    appId: "1:450230086080:web:9c3610867bd4a37a574025",
-    measurementId: "G-M43NFXQ6RN"
-}
-
 firebase.initializeApp(config);
 
 var dest = document.querySelector("#root");
